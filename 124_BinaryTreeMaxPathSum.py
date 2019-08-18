898e25bbe88424034a438fb68efb12e18599feb1
mayu0007 <yuema1992@yahoo.com>
2019-08-18 23:41:55 +0800

    Task 11: need to revisit

---
+++ 124_BinaryTreeMaxPathSum.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 18 03:11:06 2019
+124. Binary Tree Maximum Path Sum
+@author: my
+"""
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
+
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        def max_gain(node):
+            nonlocal max_sum
+            if not node:
+                return 0
+
+            # max sum on the left and right sub-trees of node
+            left_gain = max(max_gain(node.left), 0)
+            right_gain = max(max_gain(node.right), 0)
+            
+            # the price to start a new path where `node` is a highest node
+            price_newpath = node.val + left_gain + right_gain
+            
+            # update max_sum if it's better to start a new path
+            max_sum = max(max_sum, price_newpath)
+        
+            # for recursion :
+            # return the max gain if continue the same path
+            return node.val + max(left_gain, right_gain)
+   
+        max_sum = float('-inf')
+        max_gain(root)
+        return max_sum
